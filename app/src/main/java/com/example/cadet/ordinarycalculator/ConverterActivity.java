c3363b8161c3b7b9be7c1dcae512654cac1bb7d7
Michael <hitonebest@gmail.com>
2018-10-25 00:00:38 +0300

    add elements on converter layout

--- app/src/main/java/com/example/cadet/ordinarycalculator/ConverterActivity.java
+++ app/src/main/java/com/example/cadet/ordinarycalculator/ConverterActivity.java
@@ -2,6 +2,7 @@ package com.example.cadet.ordinarycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class ConverterActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ConverterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
+
+        Spinner spinner = (Spinner) findViewById(R.id.type_spinner);
+
+
     }
 }
